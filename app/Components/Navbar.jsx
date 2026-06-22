0e35a8fb9b150a0581caedb535101e0817f900a2
Youssef <ym849447@gmail.com>
2026-06-22 10:05:45 +0300

    adjust navbar

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -1,13 +1,14 @@
 "use client";
+
 import Link from "next/link";
-import { useContext, useState } from "react";
+import { useState } from "react";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="sticky top-0 grid grid-cols-2 md:grid-cols-3 items-center bg-white font-bold text-purple-600 p-3 z-50 border-b border-gray-200 dark:border-neutral-700 shadow-lg">
-      <h3 className="text-2xl">Teqneia</h3>
+    <nav className="sticky top-0 grid grid-cols-2 md:grid-cols-3 items-center bg-white font-bold text-gray-500 p-3 z-50 border-b border-gray-200 dark:border-neutral-700 shadow-lg">
+      <h3 className="text-2xl text-purple-600">Teqneia</h3>
 
       <div className="hidden md:flex gap-4 justify-center">
         <a href="#home" className="hover:text-purple-600 transition-all">
@@ -58,31 +59,31 @@ export default function Navbar() {
       </div>
 
       {isOpen && (
-        <div className="md:hidden fixed top-16 left-0 w-screen bg-purple-950/50 border-t border-gray-200 dark:border-neutral-700 shadow-lg z-50">
+        <div className="md:hidden fixed top-16 left-0 w-screen bg-neutral-200/50 border-t border-gray-200 dark:border-neutral-700 shadow-lg z-50">
           <a
             href="#home"
-            className="block px-6 py-4 text-white hover:bg-purple-950"
+            className="block px-6 py-4 text-purple-600 hover:bg-purple-700 hover:text-white transition-all duration-200"
             onClick={() => setIsOpen(false)}
           >
             Home
           </a>
           <a
             href="#about"
-            className="block px-6 py-4 text-white hover:bg-purple-950"
+            className="block px-6 py-4 text-purple-600 hover:bg-purple-700 hover:text-white transition-all duration-200"
             onClick={() => setIsOpen(false)}
           >
             About
           </a>
           <a
             href="#contact"
-            className="block px-6 py-4 text-white hover:bg-purple-950"
+            className="block px-6 py-4 text-purple-600 hover:bg-purple-700 hover:text-white transition-all duration-200"
             onClick={() => setIsOpen(false)}
           >
             Contact
           </a>
           <Link
             href="/login"
-            className="block px-6 py-4 text-white hover:bg-purple-950"
+            className="block px-6 py-4 text-black hover:bg-purple-700 hover:text-white transition-all duration-200"
             onClick={() => setIsOpen(false)}
           >
             Login
